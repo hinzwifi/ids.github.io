828516060f8f9985857aca4a89d783f77283163e
hinzwifi <69793444+hinzwifi@users.noreply.github.com>
2021-01-12 08:45:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,14 +6,3 @@ tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 
-
-
-var mymap = L.map('mapid').setView([8.239889, 124.243552], 13);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'sk.eyJ1IjoiaGluendpZmkiLCJhIjoiY2tqdDkwZm8zMDBqODJ6bzg1dHIwdTlieSJ9.D_mQgMCmCn_cq69GQH8vsw'
-}).addTo(mymap);
