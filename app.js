5cf3e4c0a7238a652c5d410c73b380bf9c44bf11
hinzwifi <69793444+hinzwifi@users.noreply.github.com>
2021-01-22 15:07:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -35,3 +35,17 @@ $(window).on("load", function() {
     $(".se-pre-con").fadeOut("slow");;
 });
   
+
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
